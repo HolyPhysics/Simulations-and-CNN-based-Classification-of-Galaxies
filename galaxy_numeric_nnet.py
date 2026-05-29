a227890e7bc16660f6d741832a62ddc019b49cba
Chidiebere Okafor <cnokaf@gmail.com>
2026-05-29 16:50:46 -0400

    Starting with numeric classification of galaxies using neural network.

---
+++ galaxy_numeric_nnet.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+# print(torch.version)
+
+# a little review for myself
+
+batch_number, input_size, hidden_layer_size, output_size = 100, 2, 50, 1
+
+# input test tensors
