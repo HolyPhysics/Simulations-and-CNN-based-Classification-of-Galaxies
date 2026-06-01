be2e82b08e7637c6543357efaeed69be1591b81b
Chidiebere Okafor <cnokaf@gmail.com>
2026-06-01 15:19:00 -0400

    started the neural network architecture.

--- galaxy_numeric_nnet.py
+++ galaxy_numeric_nnet.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import matplotlib.pyplot as plt
 # print(torch.version)
 
 # a little review for myself
@@ -7,3 +8,41 @@ import torch.nn as nn
 batch_number, input_size, hidden_layer_size, output_size = 100, 2, 50, 1
 
 # input test tensors
+x = torch.randn(batch_number, input_size)*3.1415 # This creates a "batch_number * input_size" tensor of dimension 2 (matrix)
+# print(x.ndim)
+# print(type(x).__name__)
+# print(x.requires_grad)
+y = ( x[:,0].sin() + x[:,1].cos() ) # this is still a vector with 100 columns
+y = y.unsqueeze(1) # This adds an extra dimension at index 1, resulting in a "batch_number * output_size" tensor of dimension 2(matrix)
+# print(y)
+noise = torch.randn(batch_number, output_size)*1e-1
+# print(noise)
+y = y + noise # This makes learning the neural network work to learn the function y = sin(x_0) + cos(x_1)
+# print(y)
+plt.style.use("Solarize_Light2") # Sets a color theme for the background
+figure, (ax_one, ax_two)= plt.subplots(1,2,figsize=(8.5,7.5))
+ax_one.scatter(x.numpy()[:,0], x.numpy()[:,1], c=(y-noise).numpy()[:]) # Change is visible fomr the color grading
+ax_one.set_xlim(-10,10)
+ax_one.set_ylim(-10, 10)
+ax_one.set_xlabel("Scatter diagram for actual function")
+
+ax_two.scatter(x.numpy()[:,0], x.numpy()[:,1], c=y.numpy()[:])
+ax_two.set_xlim(-10,10)
+ax_two.set_ylim(-10, 10)
+ax_two.set_xlabel("Scatter diagram for function with noise")
+figure.tight_layout()
+plt.show()
+
+
+
+class TwoLayerNet(nn.Module):
+    def __init__(self, input_size, hidden_layer_size, ouput_size) -> None:
+        super().__init__()
+        self.first_linear_layer = nn.Linear(input_size, hidden_layer_size)
+        self.activation_function = nn.Sigmoid()
+        self.second_linear_layer = nn.Linear(hidden_layer_size, ouput_size)
+
+    def forward(self, X) -> list[float]: ## Forward must always be defined and it outlines the order the feedforward mechanism follows during the training of the neural network.
+        output = self.first_linear_layer(X)
+        output = self.activation_function(output) ## This ouput is different from the one above.
+        y_pred = self.second_linear_layer(output)
