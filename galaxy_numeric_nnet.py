8d1c125beea9e64bdecf8de557230843df6a5c09
Chidiebere Okafor <cnokaf@gmail.com>
2026-06-01 16:16:07 -0400

    implemented the training loop.

--- galaxy_numeric_nnet.py
+++ galaxy_numeric_nnet.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 # a little review for myself
 
-batch_number, input_size, hidden_layer_size, output_size = 100, 2, 50, 1
+batch_number, input_size, hidden_layer_size, output_size = 1000, 2, 50, 1
 
 # input test tensors
 x = torch.randn(batch_number, input_size)*3.1415 # This creates a "batch_number * input_size" tensor of dimension 2 (matrix)
@@ -42,7 +42,56 @@ class TwoLayerNet(nn.Module):
         self.activation_function = nn.Sigmoid()
         self.second_linear_layer = nn.Linear(hidden_layer_size, ouput_size)
 
-    def forward(self, X) -> list[float]: ## Forward must always be defined and it outlines the order the feedforward mechanism follows during the training of the neural network.
+    def forward(self, X) -> list[float]: ## Forward must always be defined as it outlines the sequence/order the feedforward mechanism follows during the training of the neural network.
         output = self.first_linear_layer(X)
         output = self.activation_function(output) ## This ouput is different from the one above.
         y_pred = self.second_linear_layer(output)
+        return y_pred
+
+''' For ease of the optimization, I'm going to assume that the function the neural network 
+will learn is convex at all value of the input so we can take negative gradient steps. 
+To start the optimizartion and training, we initialize an instance of the model '''
+
+model = TwoLayerNet(input_size, hidden_layer_size, output_size)
+
+''' And we choose a loss function. For convenience, I'll go with the Mean Squared Error(MSE) function '''
+loss_function = nn.MSELoss(reduction="sum")
+learning_rate = 1e-4
+
+for values in range(batch_number+1):
+    '''Forward pass: Computes the predicted values of y by passing x to the model. It is
+    important to note that the Module object overrides the __call__ operator so one call 
+    them like functions. That is we can use: "model(x)". That is, one can pass tensors of input
+    data to the Module and it produces a tensor of output data 
+    '''
+    y_pred = model(x) # Recall that x is defined above
+
+    '''Now, we compute and print the loss. We just need to pass the predicted value and the true values
+    and the loss function returns a tensor containinf the loss.'''
+    loss = loss_function(y_pred, y) ## Again, recall y is defined above with noises
+    if values%100 == 0:
+        print(f'{values}-th iteration, loss: {loss.item()}') # loss.item() gives us the returned value for the loss(this is a single value for each iteration)
+
+    '''The next step is to zero all involved gradients before we proceed to run the backward pass
+    which wll compute the gradient for each iteration '''
+    model.zero_grad()
+
+    '''Backward pass: This will compute the gradient of the loss with respect to all the
+    learnable parameters of the model. Internally, the parameters of each Module aare stored
+    tensors with "requires_grad=True", so this call computes gradients for all learnable
+    parameters in the model. '''
+
+    loss.backward() # The gradient points in direction of steepest ascent. Easily follows from vector calculus
+
+    ''' Update weights using gradient descent. Each parameter is a tensor, so we can access its gradients
+    like we did before '''
+
+    with torch.no_grad(): 
+        ''' torch.no_grad() simply stops further tracking/computation of gradients this does not clear 
+        the gradients. Use optimizer.zero_grad() or model.zero_grad() to clear the gradients '''
+
+        for param in model.parameters():
+            param -= learning_rate*param.grad 
+            ''' We are able to take negative step because we have assumed 
+            the function is everywhere convex. Of course, this may not be the case. In such cases, we can use optimizers
+            like Adam, etc.'''
